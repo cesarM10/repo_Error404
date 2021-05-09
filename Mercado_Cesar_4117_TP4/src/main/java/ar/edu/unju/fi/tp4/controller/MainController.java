6803f06ceee405844a39c037c687ea4581a0bf06
cesarmercadook <cesarmercadook@gmail.com>
2021-05-09 19:58:31 -0300

    Correccion model Compra. Añadi Metodo para calcular el total por compra.
    Responsable: Mercado, Cesar David

--- Mercado_Cesar_4117_TP4/src/main/java/ar/edu/unju/fi/tp4/controller/MainController.java
+++ Mercado_Cesar_4117_TP4/src/main/java/ar/edu/unju/fi/tp4/controller/MainController.java
@@ -5,6 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+	/* Integrantes del Grupo Error404:
+	 * Gaspar, Alvaro Emanuel
+	 * Mercado, Cesar David
+	 * Rodriguez, Enrique Sebastian
+	 * Salas, Ivan Arnaldo
+	 */
+	
 	@GetMapping("/home")
 	public String getPageHome() {
 		return "home";
