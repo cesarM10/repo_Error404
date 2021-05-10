b13813ff5e1e4ab7e5da0924090130a420395cb9
quique <quique@DESKTOP-JO4K93B>
2021-05-10 01:22:39 -0300

    Tarea: Creacion del template formulario-compra.html y lista-compras.html. Responsable: Rodriguez, Enrique Sebastian.

--- Mercado_Cesar_4117_TP4/src/main/java/ar/edu/unju/fi/tp4/service/imp/ClienteServiceImp.java
+++ Mercado_Cesar_4117_TP4/src/main/java/ar/edu/unju/fi/tp4/service/imp/ClienteServiceImp.java
@@ -21,7 +21,9 @@ public class ClienteServiceImp implements IClienteService {
 	public void generarTablaCliente() {
 		clienteList = TablaCliente.listaClientes;
 		clienteList.add(new Cliente("DNI",1234,"Cesar Mercado","cesarm10@gmail.com","01234",LocalDate.of(1992, 04, 10),3886,617729,LocalDate.of(2021, 03, 17)));
-		clienteList.add(new Cliente("Pasaporte",5678,"David Mercado","example@gmail.com","98765",LocalDate.of(1993, 07, 8),3886,849151,LocalDate.of(2021, 04, 10)));
+		clienteList.add(new Cliente("Pasaporte",9987,"Enrique Rodriguez","enrique@gmail.com","9122018",LocalDate.of(1994, 07, 3),388,765234,LocalDate.of(2020, 01, 10)));
+		clienteList.add(new Cliente("DNI",5467,"Ivan Salas","ivan123@gmail.com","pass123",LocalDate.of(1997, 12, 1),355,543643,LocalDate.of(2021, 05, 10)));
+		clienteList.add(new Cliente("Pasaporte",6744,"Gaspar Alvaro","alvaro678@gmail.com","P677ss",LocalDate.of(1995, 01, 13),399,56246,LocalDate.of(2018, 12, 9)));
 		LOGGER.info("METHOD: generarTablaCliente - creo primer cliente por defecto" + clienteList.get(clienteList.size()-1));
 
 	}
